f85e543e043204b80af3809b977932ddf232793d
i8086m <35865583+i8086m@users.noreply.github.com>
2022-07-06 19:58:40 +0300

    implement new features (export to files, styles, ...)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QSlider, QHBoxLayout
+from PyQt5.QtWidgets import QSlider, QHBoxLayout, QPushButton, QVBoxLayout, QRadioButton, QLabel, QButtonGroup, \
+    QPlainTextEdit
 from PyQt5 import QtGui, QtWidgets
 
 import sys
@@ -19,7 +20,12 @@ class App(QtWidgets.QWidget):
     slider_resolution = 60
     frame_width = 640
     frame_height = int(frame_width / get_video_aspect_ratio(video))
+
     hist_width = 256 * 3
+    hist_color = 'red'
+    hist_style = 'default'
+    gray_img = None
+    hist_img = None
 
     def show_image(self):
         frame = frame_from_video(self.video, self.slider.value() / self.slider_resolution)
@@ -27,31 +33,115 @@ class App(QtWidgets.QWidget):
         img = resize_image(frame, self.frame_width, self.frame_height)
         set_image(self.image_frame, img)
 
-        gray_img = bgr2gray(img)
-        set_image(self.image_gray_frame, gray2rgb(gray_img))
+        self.gray_img = bgr2gray(img)
+        set_image(self.image_gray_frame, gray2rgb(self.gray_img))
 
-        hist_img = get_hist(gray_img, self.hist_width, self.frame_height)
-        set_image(self.image_hist_frame, hist_img)
+        self.hist_img = get_hist(
+            self.gray_img,
+            self.hist_width,
+            self.frame_height,
+            style=self.hist_style,
+            color=self.hist_color,
+        )
+        set_image(self.image_hist_frame, self.hist_img)
+
+    def edit_theme(self, style: str = None, color: str = None):
+        if style:
+            self.hist_style = style
+        if color:
+            self.hist_color = color
+        self.show_image()  # update the histogram
+
+    def export_grayscale(self, mode: int = 0):
+        img = self.gray_img
+        rows, cols = img.shape
+
+        if mode == 0:  # as Bytes
+            with open('exported.txt', 'wb') as file:
+                file.write(img.tobytes())
+        elif mode == 1:  # as Numbers
+            with open('exported.txt', 'w') as file:
+                for y in range(rows):
+                    file.write(' '.join([str(img[y, x]) for x in range(cols)]) + '\n')
+        else:
+            raise ValueError('Invalid export mode')
+        self.log.appendPlainText('Изображение экспортировано')
+
+    def save_hist(self):
+        if self.hist_img is not None:
+            cv2.imwrite('output.png', self.hist_img)
+        self.log.appendPlainText('Гистограмма сохранена')
 
     def __init__(self, parent=None):
         super(App, self).__init__(parent)
         self.setWindowTitle('PyHistogramViewer [MAI]')
 
         self.image_thread = QtThread(target=self.show_image)
-        print(self.frame_width, self.frame_height)
 
         hbox = QHBoxLayout()
-
         self.image_frame = QtWidgets.QLabel()
         self.image_frame.setAlignment(Qt.AlignLeft)
         self.image_gray_frame = QtWidgets.QLabel()
         self.image_gray_frame.setAlignment(Qt.AlignRight)
-
         hbox.addWidget(self.image_frame)
         hbox.addWidget(self.image_gray_frame)
 
+        hbox2 = QHBoxLayout()
         self.image_hist_frame = QtWidgets.QLabel()
         self.image_hist_frame.setAlignment(Qt.AlignCenter)
+
+        vbox_left = QVBoxLayout()
+        self.btngroup1 = QButtonGroup()
+        self.btngroup2 = QButtonGroup()
+        self.style_lbl = QLabel("Select style")
+        self.style_rdbtn1 = QRadioButton("Default")
+        self.style_rdbtn1.setChecked(True)
+        self.style_rdbtn1.toggled.connect(lambda: self.edit_theme(style='default'))
+        self.style_rdbtn2 = QRadioButton("Alternative")
+        self.style_rdbtn2.toggled.connect(lambda: self.edit_theme(style='alternative'))
+        self.color_lbl = QLabel("Select color")
+        self.color_rdbtn_red = QRadioButton("Red")
+        self.color_rdbtn_red.setChecked(True)
+        self.color_rdbtn_red.toggled.connect(lambda: self.edit_theme(color='red'))
+        self.color_rdbtn_green = QRadioButton("Green")
+        self.color_rdbtn_green.toggled.connect(lambda: self.edit_theme(color='green'))
+        self.color_rdbtn_blue = QRadioButton("Blue")
+        self.color_rdbtn_blue.toggled.connect(lambda: self.edit_theme(color='blue'))
+        self.btngroup1.addButton(self.style_rdbtn1)
+        self.btngroup1.addButton(self.style_rdbtn2)
+        self.btngroup2.addButton(self.color_rdbtn_red)
+        self.btngroup2.addButton(self.color_rdbtn_green)
+        self.btngroup2.addButton(self.color_rdbtn_blue)
+        vbox_left.addStretch(1)
+        vbox_left.addWidget(self.style_lbl)
+        vbox_left.addWidget(self.style_rdbtn1)
+        vbox_left.addWidget(self.style_rdbtn2)
+        vbox_left.addWidget(self.color_lbl)
+        vbox_left.addWidget(self.color_rdbtn_red)
+        vbox_left.addWidget(self.color_rdbtn_green)
+        vbox_left.addWidget(self.color_rdbtn_blue)
+        vbox_left.addStretch(1)
+
+        vbox_right = QVBoxLayout()
+        self.btn_save_hist = QPushButton('Save histogram as image')
+        self.btn_export_bytes = QPushButton('Export as Bytes')
+        self.btn_export_numbers = QPushButton('Export as Numbers')
+        self.btn_save_hist.clicked.connect(self.save_hist)
+        self.btn_export_bytes.clicked.connect(lambda: self.export_grayscale(mode=0))
+        self.btn_export_numbers.clicked.connect(lambda: self.export_grayscale(mode=1))
+        self.log = QPlainTextEdit()
+        self.log.setReadOnly(True)
+        self.log.setMinimumSize(50, 100)
+        vbox_right.addStretch(1)
+        vbox_right.addWidget(self.btn_save_hist)
+        vbox_right.addWidget(self.btn_export_bytes)
+        vbox_right.addWidget(self.btn_export_numbers)
+        vbox_right.addWidget(self.log)
+        vbox_right.addStretch(1)
+
+        hbox2.addLayout(vbox_left)
+        hbox2.addWidget(self.image_hist_frame)
+        hbox2.addLayout(vbox_right)
 
         self.slider = QSlider(Qt.Horizontal)
         self.slider.setMinimum(0)
@@ -61,7 +151,8 @@ class App(QtWidgets.QWidget):
 
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addLayout(hbox)
-        self.layout.addWidget(self.image_hist_frame)
+        self.layout.addLayout(hbox2)
+        # self.layout.addWidget(self.image_hist_frame)
         self.layout.addWidget(self.slider)
 
         self.setLayout(self.layout)
--- utils/image.py
+++ utils/image.py
@@ -14,17 +14,32 @@ def gray2rgb(img: np.ndarray) -> np.ndarray:
     return cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
 
-def get_hist(gray_img: np.ndarray, hist_w: int, hist_h: int) -> np.ndarray:
+def get_color_from_name(name: str):
+    if name == 'red':
+        return (0, 0, 255)
+    if name == 'green':
+        return (0, 255, 0)
+    if name == 'blue':
+        return (255, 0, 0)
+    return (255, 255, 255)
+
+
+def get_hist(gray_img: np.ndarray, hist_w: int, hist_h: int, style: str = 'default', color: str = 'red') -> np.ndarray:
     hist = cv2.calcHist([gray_img], [0], None, [256], [0, 256])
     cv2.normalize(hist, hist, alpha=0, beta=hist_h, norm_type=cv2.NORM_MINMAX)
     bin_w = int(round(hist_w / 256))
 
     hist_img = np.zeros((hist_h, hist_w, 3), dtype=np.uint8)
+    color_tuple = get_color_from_name(color)
 
     for i in range(0, 256):
-        cv2.line(hist_img, (bin_w * (i - 1), hist_h - int(hist[i - 1])),
-                 (bin_w * i, hist_h - int(hist[i])),
-                 (0, 0, 255), thickness=1)
-        #cv2.rectangle(hist_img, (int(i * bin_w), hist_h - int(hist[i][0])), (int(i * bin_w + bin_w - 1), hist_h),
-        #              (0, 0, 255), -1)
+        if style == 'default':
+            cv2.line(hist_img, (bin_w * (i - 1), hist_h - int(hist[i - 1])),
+                     (bin_w * i, hist_h - int(hist[i])),
+                     color_tuple, thickness=1)
+        elif style == 'alternative':
+            cv2.rectangle(hist_img, (int(i * bin_w), hist_h - int(hist[i][0])), (int(i * bin_w + bin_w - 1), hist_h),
+                          color_tuple, -1)
+        else:
+            raise NameError('Invalid style selected')
     return hist_img
